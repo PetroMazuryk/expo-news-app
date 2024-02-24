4977274748ec1d39fb89bd50a9445defde2be99c
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-02-24 14:41:42 +0200

    displaying the title at the top of the article

--- screens/FullPostScreen.jsx
+++ screens/FullPostScreen.jsx
@@ -3,13 +3,16 @@ import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Image, Text } from "react-native";
 import { Loading } from "../components/Loading";
 
-export const FullPostScreen = ({ route }) => {
+export const FullPostScreen = ({ route, navigation }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState();
   const { id, title } = route.params;
 
   useEffect(() => {
     setIsLoading(true);
+    navigation.setOptions({
+      title,
+    });
     axios
       .get("https://642f4410b289b1dec4b00ecd.mockapi.io/api/v1/articles/" + id)
       .then(({ data }) => {
